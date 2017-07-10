47d9b67fcb6923b9e7a1b1adb4420d6b0b6ab87b
bog5651 <div98@mail.ru>
2017-07-10 15:12:14 +0600

    addListner

--- calcularot/Form1.Designer.cs
+++ calcularot/Form1.Designer.cs
@@ -35,7 +35,10 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
@@ -61,55 +64,88 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(27, 107);
+            this.button1.Location = new System.Drawing.Point(0, 155);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(35, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "+";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(109, 106);
+            this.button2.Location = new System.Drawing.Point(41, 155);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(38, 23);
             this.button2.TabIndex = 4;
             this.button2.Text = "-";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(191, 105);
+            this.button3.Location = new System.Drawing.Point(85, 155);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(37, 23);
             this.button3.TabIndex = 5;
             this.button3.Text = "*";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(55, 137);
+            this.button4.Location = new System.Drawing.Point(128, 155);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.Size = new System.Drawing.Size(43, 23);
             this.button4.TabIndex = 6;
             this.button4.Text = "/";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
+            // label1
             // 
-            this.button5.Location = new System.Drawing.Point(156, 136);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "=";
-            this.button5.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(122, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "B";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(172, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(122, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "=";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -133,7 +169,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- calcularot/Form1.cs
+++ calcularot/Form1.cs
@@ -16,5 +16,74 @@ namespace calcularot
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double FirstArgumeng = Convert.ToDouble(textBox1.Text);
+                double SecondArgument = Convert.ToDouble(textBox1.Text);
+                double Result = FirstArgumeng + SecondArgument;
+                textBox3.Text = "+";
+                textBox4.Text = Convert.ToString(Result);
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "что-то пошло не так";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double FirstArgumeng = Convert.ToDouble(textBox1.Text);
+                double SecondArgument = Convert.ToDouble(textBox1.Text);
+                double Result = FirstArgumeng - SecondArgument;
+                textBox3.Text = "-";
+                textBox4.Text = Convert.ToString(Result);
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "что-то пошло не так";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double FirstArgumeng = Convert.ToDouble(textBox1.Text);
+                double SecondArgument = Convert.ToDouble(textBox1.Text);
+                double Result = FirstArgumeng * SecondArgument;
+                textBox3.Text = "*";
+                textBox4.Text = Convert.ToString(Result);
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "что-то пошло не так";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double FirstArgumeng = Convert.ToDouble(textBox1.Text);
+                double SecondArgument = Convert.ToDouble(textBox1.Text);
+                double Result = FirstArgumeng / SecondArgument;
+                textBox3.Text = "/";
+                textBox4.Text = Convert.ToString(Result);
+            }
+            catch (Exception)
+            {
+                textBox4.Text = "что-то пошло не так";
+            }
+        }
     }
 }
