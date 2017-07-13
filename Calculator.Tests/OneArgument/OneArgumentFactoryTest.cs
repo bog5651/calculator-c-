eb2690e1ab3cd086fe46506cc965481312e48134
bog5651 <div98@mail.ru>
2017-07-13 10:21:44 +0600

    Add test fabric

---
+++ Calculator.Tests/OneArgument/OneArgumentFactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using calcularot.OneArgument;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgument
+{
+    [TestFixture]
+    public class OneArgumentFactoryTest
+    {
+        [TestCase("cos", typeof(Cos))]
+        [TestCase("sin", typeof(Sin))]
+        [TestCase("tan", typeof(Tan))]
+        public void CalculateTest(string testName, Type expected)
+        {
+            var calculator = OneArgumentFactory.CreatCalculation(testName);
+            Assert.AreEqual(expected, calculator.GetType());
+        }
+    }
+}
---
+++ Calculator.Tests/TwoArguments/TwoArgumentFactoryTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using calcularot.OneArgument;
+using calcularot.TwoArguments;
+using NUnit.Framework;
+
+namespace Calculator.Tests.TwoArguments
+{
+    [TestFixture]
+    public class TwoArgumentFactoryTest
+    {
+        [TestCase("Plus", typeof(Addition))]
+        [TestCase("Multiplire", typeof(Multiplication))]
+        [TestCase("Division", typeof(Division))]
+        public void CalculateTest(string testName, Type expected)
+        {
+            var calculator = TwoArgumentFactory.CreatCalculation(testName);
+            Assert.AreEqual(expected, calculator.GetType());
+        }
+    }
+}
