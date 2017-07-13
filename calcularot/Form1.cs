8cc93d90a7dd9193647331225dd717cc4540a58f
bog5651 <div98@mail.ru>
2017-07-13 10:58:37 +0600

    Add try-Catch

--- calcularot/Form1.cs
+++ calcularot/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mime;
 using System.Windows.Forms;
 using calcularot.OneArgument;
 using calcularot.TwoArguments;
@@ -14,19 +15,33 @@ namespace calcularot
 
         private void ListenerTwoArgumentFunction(object sender, EventArgs e)
         {
-            double firstArgument = Convert.ToDouble(textBox1.Text);
-            double secondArgument = Convert.ToDouble(textBox2.Text);
-            ITwoArgumentsCalculation calculator = TwoArgumentFactory.CreatCalculation(((Button)sender).Name);
-            var result = calculator.Calculate(firstArgument, secondArgument);
-            textBox4.Text = result.ToString();
+            try
+            {
+                double firstArgument = Convert.ToDouble(textBox1.Text);
+                double secondArgument = Convert.ToDouble(textBox2.Text);
+                ITwoArgumentsCalculation calculator = TwoArgumentFactory.CreatCalculation(((Button) sender).Name);
+                var result = calculator.Calculate(firstArgument, secondArgument);
+                textBox4.Text = result.ToString();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Что-то пошло не так" + exc);
+            }
         }
 
         private void ListenerOneArgumentsFunction(object sender, EventArgs e)
         {
-            double firstArgument = Convert.ToDouble(textBox1.Text);
-            IOneArgumentsCalculation calculator = OneArgumentFactory.CreatCalculation(((Button)sender).Name);
-            var result = calculator.Calculate(firstArgument);
-            textBox4.Text = result.ToString();
+            try
+            {
+                double firstArgument = Convert.ToDouble(textBox1.Text);
+                IOneArgumentsCalculation calculator = OneArgumentFactory.CreatCalculation(((Button) sender).Name);
+                var result = calculator.Calculate(firstArgument);
+                textBox4.Text = result.ToString();
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show("Что-то пошло не так" + exc);
+            }
         }
     }
 }
